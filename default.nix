33f1d95776693425b994daeb1552187ae5525836
Paul Gray <paul.gray@instructure.com>
2023-05-11 11:03:31 -0400

    Fixing for newer nixpkgs versions

--- default.nix
+++ default.nix
@@ -10,13 +10,16 @@
     '';
   };
 
-  vscode-extension = vscode-utils.buildVscodeExtension {
+  vscode-extension = vscode-utils.buildVscodeExtension rec {
 
     name = "gititles";
     src = "${build.out}/libexec/gitiles/deps/gitiles"; # todo: src should be the output directory I think?
     # Same as "Unique Identifier" on the extension's web page.
     # For the moment, only serve as unique extension dir.
-    vscodeExtUniqueId = "pfgray.gitiles";
+    vscodeExtPublisher = "pfgray";
+    vscodeExtName = "gitiles";
+    vscodeExtUniqueId = "${vscodeExtPublisher}.${vscodeExtName}";
+    version = "0.0.1";
 
     nativeBuildInputs = [yarn];
 
